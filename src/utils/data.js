9bb021abbec4d035917a790ba77bad0e698a2284
putri <putrisanjiwanidewi@gmail.com>
2022-01-21 21:20:54 +0900

    data

---
+++ src/utils/data.js
@@ -0,0 +1,36 @@
+import Novel from "../assets/books/Novel.png"
+import Pengetahuan from "../assets/books/Pengetahuan.png"
+import Resep from "../assets/books/Resep-Makanan.png"
+import Sejarah from "../assets/books/Sejarah.png"
+
+export const products =[
+    {
+        id: 1,
+        name: "Mengenang Namanya",
+        image:Novel,
+        category: "Novel",
+        price:85000
+    },
+    {
+        id:2,
+        name: "Cetak Biru dan Bangunan",
+        image: Pengetahuan,
+        category: "Pengetahuan",
+        price:75000
+    },
+    {
+        id: 3,
+        name: "Resep Makanan",
+        image:Resep,
+        category: "Resep",
+        price:60000
+    },
+    {
+        id: 4,
+        name: "Sejarah Romawi",
+        image:Sejarah,
+        category: "Sejarah",
+        price:105000
+    },
+
+]
